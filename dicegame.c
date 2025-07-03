6aae9dfe9ce503596ad76934a8f6202ec28e717b
“kitada_ayumu” <kitada_ayumu@plise.co.jp>
2025-07-03 12:44:27 +0900

    Won/lost message added

--- dicegame.c
+++ dicegame.c
@@ -1,15 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main (){
+int main() {
     char name[100];
-    
-    printf("What is your name?\n");
+    int die1, die2, total;
 
+    printf("What is your name?\n");
     scanf("%99s", name);
 
     printf("Hello, %s!\n", name);
 
+    // 乱数の種を初期化
+    srand(time(NULL));
 
+    // サイコロを振る
+    die1 = rand() % 6 + 1;
+    die2 = rand() % 6 + 1;
+    total = die1 + die2;
+
+    // サイコロの結果を表示
+    printf("Rolling dice...\n");
+    printf("Die 1: %d\n", die1);
+    printf("Die 2: %d\n", die2);
+    printf("Total value: %d\n", total);
+
+    // 勝敗の判定
+    if (total > 7) {
+        printf("You won!\n");
+    } else {
+        printf("You lose\n");
+    }
 
     return 0;
 }
