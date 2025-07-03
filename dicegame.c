9bd9b706b15f8e42eaea994899bc96f259244ffa
“kitada_ayumu” <kitada_ayumu@plise.co.jp>
2025-07-03 12:47:11 +0900

    Won/lost message modified”

--- dicegame.c
+++ dicegame.c
@@ -11,21 +11,18 @@ int main() {
 
     printf("Hello, %s!\n", name);
 
-    // 乱数の種を初期化
     srand(time(NULL));
 
-    // サイコロを振る
     die1 = rand() % 6 + 1;
     die2 = rand() % 6 + 1;
     total = die1 + die2;
 
-    // サイコロの結果を表示
     printf("Rolling dice...\n");
     printf("Die 1: %d\n", die1);
     printf("Die 2: %d\n", die2);
     printf("Total value: %d\n", total);
 
-    // 勝敗の判定
+
     if (total > 7) {
         printf("You won!\n");
     } else {
